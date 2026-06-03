cd00d8d54565de12f26f5913888bbadde22087d2
Kushal <harshilkushal100@gmail.com>
2026-06-03 17:11:44 +0530

    Added Collison Between Particles and Rigid Bodies

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from simulation.world import World
 
 world = World(800, 600)
 renderer = Renderer(world)
-# p1 = Particle(0, 100, 1.0)
+p1 = Particle(0, 100, 1.0)
 # p2 = Particle(600, 800, 10.0)
 # spring = Spring(p1 , p2 , length=100, k=0.1, damp=0.5)
 body = RigidBody(400, 300, 1.0, 0.0)
