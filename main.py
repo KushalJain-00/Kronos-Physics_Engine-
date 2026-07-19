2069fb7b117d98ff82bdd58abd803a3f9ac28c3a
Kushal Jain <harshilkushal100@gmail.com>
2026-07-19 12:41:34 +0530

    Conpleted chain constraint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core.particles import Particle
 from core.springs import Spring
 from core.rigidbody import RigidBody
-from core.constraints import DistanceConstraint , HingeConstraint
+from core.constraints import DistanceConstraint , HingeConstraint , ChainConstraint
 from visualization.renderer import Renderer
 from simulation.world import World
 from ui.control_panel import ControlPanel
@@ -10,23 +10,23 @@ world = World(800, 600)
 panel = ControlPanel(world)
 renderer = Renderer(world)
 
-body = RigidBody(400, 300, 1.0, 0.0)
+body = RigidBody(200, 100, 1.0, 0.0)
 body.set_shape([(-50,-25),(50,-25),(50,25),(-50,25)])
 body.angular_velocity = 1.0
 body2 = RigidBody(450, 350, 1.0, 0.0)
 body2.set_shape([(-50,-25),(50,-25),(50,25),(-50,25)])
-body2.angular_velocity = 1.0 
-body3 = RigidBody(500, 300, 1.0, 0.0)
-body3.set_shape([(-50,-25),(50,-25),(50,25),(-50,25)])
-body3.angular_velocity = 1.0
+# body2.angular_velocity = 1.0 
+# body3 = RigidBody(500, 300, 1.0, 0.0)
+# body3.set_shape([(-50,-25),(50,-25),(50,25),(-50,25)])
+# body3.angular_velocity = 1.0
 
-constraint = HingeConstraint(body , body2 , (50,25) , (25,-25))
-constraint2 = HingeConstraint(body2 , body3 , (-50,25) , (-50,25))
+constraint = DistanceConstraint(body , body2 , (50,25) , (25,-25) , 100)
+# constraint2 = ChainConstraint(world , body2 , body3 , (-50,25) , (-50,25) , visible_links=True , n_links=5)
 
 world.add_rigid_bodies(body)
 world.add_rigid_bodies(body2)
-world.add_rigid_bodies(body3)
+# world.add_rigid_bodies(body3)
 world.add_constraint(constraint)
-world.add_constraint(constraint2)
+# world.add_constraint(constraint2)
 panel.start()
 renderer.run()
